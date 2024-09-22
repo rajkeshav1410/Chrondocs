338a7621ce23d2c8298ad7c0ecb831de08cb3804
Keshav Raj <www.keshav.mars@gmail.com>
2024-09-22 16:52:14 +0530

    Express backend boiler plate

---
+++ backend/src/index.ts
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+import cors, { CorsOptions } from "cors";
+import express, { Application } from "express";
+import cookieParser from "cookie-parser";
+
+const app: Application = express();
+const url = `${process.env.BASE_URL}:${process.env.PORT}`;
+
+const corsOptions: CorsOptions = {
+  origin: "*",
+};
+
+app.set("trust proxy", true);
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Server is up and running" });
+});
+
+// start the server
+app.listen(
+  parseInt(process.env.PORT || "8081", 10),
+  process.env.BASE_URL || "localhost",
+  () => {
+    console.info(`Server running: ${url}`);
+  }
+);
